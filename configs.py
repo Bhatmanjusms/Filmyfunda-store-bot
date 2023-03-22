21f9b7a036a988f279ff165e4e0dfca732db8165
Manjunath Bhat <86521081+Bhatmanjusms@users.noreply.github.com>
2023-03-22 23:53:56 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,18 +4,18 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
-	API_HASH = os.environ.get("API_HASH")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
-	DATABASE_URL = os.environ.get("DATABASE_URL")
+	API_ID = int(os.environ.get("API_ID", "977080"))
+	API_HASH = os.environ.get("API_HASH", "0c20c4265501492a1513f91755acd42b")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN", "5833998378:AAEW7ZnRmtj8IbYf_5oxMThtc-n4GivFP0w")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "filmy_funda_store_bot")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001637400311"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "399726799"))
+	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://abcd:abcd@cluster0.krtq5wm.mongodb.net/?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001637400311")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
-	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
-	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
+	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", False))
+	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
