3cb9b421a5a229706f6cef6d50d5c4d427367a5e
Teja <67798221+kangScripter@users.noreply.github.com>
2023-08-27 11:32:33 +0530

    Update shortner.py

--- handlers/shortner.py
+++ handlers/shortner.py
@@ -1,8 +1,10 @@
 import requests
+from configs import Config
 
 def short(url):
-      params = {'api':'a85e8131ddd156a0b673a35675d01c4dbee914d9', 'url': url}
-      api_url = 'https://tnshort.net/api'
+      params = {'api':Config.API_KEY,#'a85e8131ddd156a0b673a35675d01c4dbee914d9',
+                'url': url}
+      api_url = Config.API_URL,#'https://tnshort.net/api'
       res = requests.get(api_url,params=params)
       return res.json()["shortenedUrl"]
    
