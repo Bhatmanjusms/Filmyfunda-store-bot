36bf4cfe3c2eea7081efadb2a5de6bc8616ab346
Teja <67798221+kangScripter@users.noreply.github.com>
2023-08-27 11:09:52 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -7,18 +7,16 @@ from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from handlers.helpers import str_to_b64
 
-delete_delay = 2 * 60
+delete_delay = 60
 async def media_forward(bot: Client, user_id: int, file_id: int):
     try:
         if Config.FORWARD_AS_COPY is True:
-            return fmsg = await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
+            return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
                                           message_id=file_id)
         elif Config.FORWARD_AS_COPY is False:
-            return fmsg = await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
+            return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
                                               message_ids=file_id)
-        time.sleep(delete_delay)
-        msg_ig = fmsg.message_id
-        bot.delete_messages(chat_id=user_id,message_id=msg_id)
+        
     except FloodWait as e:
         await asyncio.sleep(e.value)
         return media_forward(bot, user_id, file_id)
@@ -27,3 +25,6 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
     await asyncio.sleep(2)
+    await asyncio.sleep(delete_delay)
+    msg_ig = sent_message.message_id
+    bot.delete_messages(chat_id=user_id,message_id=msg_id)
