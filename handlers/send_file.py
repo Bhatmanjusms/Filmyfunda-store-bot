eb6ddcad2cfda81ca68c358655784d235b0df51a
Teja <67798221+kangScripter@users.noreply.github.com>
2023-08-27 11:07:28 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -11,13 +11,14 @@ delete_delay = 2 * 60
 async def media_forward(bot: Client, user_id: int, file_id: int):
     try:
         if Config.FORWARD_AS_COPY is True:
-            return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
+            return fmsg = await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
                                           message_id=file_id)
         elif Config.FORWARD_AS_COPY is False:
-            return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
+            return fmsg = await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
                                               message_ids=file_id)
         time.sleep(delete_delay)
-        bot.delete_messages(chat_id=user_id,message_id=file_id)
+        msg_ig = fmsg.message_id
+        bot.delete_messages(chat_id=user_id,message_id=msg_id)
     except FloodWait as e:
         await asyncio.sleep(e.value)
         return media_forward(bot, user_id, file_id)
