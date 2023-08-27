46176233276e1c342ebb98ce6a95241a65d973c8
Teja <67798221+kangScripter@users.noreply.github.com>
2023-08-27 11:14:51 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -7,7 +7,7 @@ from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from handlers.helpers import str_to_b64
 
-delete_delay = 60
+delete_delay = 30
 async def media_forward(bot: Client, user_id: int, file_id: int):
     try:
         if Config.FORWARD_AS_COPY is True:
@@ -26,5 +26,5 @@ async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
     await asyncio.sleep(2)
     await asyncio.sleep(delete_delay)
-    msg_ig = sent_message.id
+    msg_id = sent_message.id
     bot.delete_messages(chat_id=user_id,message_id=msg_id)
