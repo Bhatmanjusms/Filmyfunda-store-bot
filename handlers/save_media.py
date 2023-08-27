eac8ae91930f02990047b0c3105c890d41897e6e
Teja <67798221+kangScripter@users.noreply.github.com>
2023-08-27 11:56:14 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -49,7 +49,7 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
                 InlineKeyboardButton("Delete Batch", callback_data="closeMessage")
             ]])
         )
-        if Config.IS_SHORTNER:
+        if Config.IS_SHORTNER is True:
            share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=thunderbot_{str_to_b64(str(SaveMessage.id))}"
            short_link = short(share_link)
         else:
@@ -90,7 +90,7 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
         await forwarded_msg.reply_text(
             f"#PRIVATE_FILE:\n\n[{message.from_user.first_name}](tg://user?id={message.from_user.id}) Got File Link!",
             disable_web_page_preview=True)
-        if Config.IS_SHORTNER:
+        if Config.IS_SHORTNER is True:
            share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=thunderbot_{str_to_b64(str(SaveMessage.id))}"
            short_link = short(share_link)
         else:
