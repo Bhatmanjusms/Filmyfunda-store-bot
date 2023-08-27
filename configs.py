dcd31d43aafd4d7322e5b034e2a6e0e45e1e9e32
Teja <67798221+kangScripter@users.noreply.github.com>
2023-08-27 11:34:03 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -18,7 +18,7 @@ class Config(object):
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
-	API_URL = ''
+	API_URL = 'https://tnshort.net/api'
 	API_KEY = ''
 	DELAY_DELETE_TIME = 60#In seconds
 	ABOUT_BOT_TEXT = f"""
