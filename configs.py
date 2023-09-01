df25689359b2b28696abad4db1f9b42fb9212f6c
Manjunath Bhat <86521081+Bhatmanjusms@users.noreply.github.com>
2023-09-01 09:15:34 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,8 +6,8 @@ import os
 class Config(object):
 	API_ID = int(os.environ.get("API_ID", "977080"))
 	API_HASH = os.environ.get("API_HASH", "0c20c4265501492a1513f91755acd42b")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN", "6358096220:AAGT3mgBRl2tGSZq9qhMnkfSlTl2NoeO_7g")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME", "thunder_files2_bot")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN", "6436297049:AAGHO5rPl91U-N5KKaQFDR9MV4V1an0yZy8")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "test_5678_bot")
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001637400311"))
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "399726799"))
 	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://abcd:abcd@cluster0.zglig1z.mongodb.net/?retryWrites=true&w=majority")
@@ -18,7 +18,7 @@ class Config(object):
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
-	IS_SHORTNER = False
+	IS_SHORTNER = True
 	API_URL = 'https://tnshort.net/api'
 	API_KEY = 'a85e8131ddd156a0b673a35675d01c4dbee914d9'
 	DELAY_DELETE_TIME = 1800 #In seconds
